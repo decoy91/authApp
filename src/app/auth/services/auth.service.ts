12aea4dc2937b8ccc578764899553be46057bcdb
Aleks <gajim91@gmail.com>
2024-11-05 23:26:31 -0600

    Servicio para verificar el token de acceso

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { sign } from 'crypto';
 import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
-import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { AuthStatus, CheckTokenResponse, LoginResponse, User } from '../interfaces';
 import { response } from 'express';
 
 @Injectable({
@@ -43,4 +43,34 @@ export class AuthService {
       )
     )
   }
+
+  checkAuthStatus():Observable<boolean>{
+
+    const url = `${ this.baseUrl }/auth/check-token`;
+
+    const token = localStorage.getItem('token');
+
+    if(!token) return of (false);
+
+    const headers = new HttpHeaders()
+    .set('Authorization', `Bearer ${ token }`)
+
+    return this.http.get<CheckTokenResponse>(url, { headers})
+    .pipe(
+      map(({token,user})=>{
+        this._currentUser.set(user);
+        this._authStatus.set(AuthStatus.authenticated);
+        localStorage.setItem('token', token);
+        return true;
+      }),
+
+      //error
+      catchError(()=> {
+        this._authStatus.set(AuthStatus.notAuthenticated)
+        return of(false);
+      })
+    )
+
+  }
+
 }
